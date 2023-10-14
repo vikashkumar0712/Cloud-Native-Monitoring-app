44f8b9706a9d0ffae2c2b9c596a2c2e55b6874b0
VIKASH KUMAR <89198300+vikashkumar0712@users.noreply.github.com>
2023-10-14 14:05:02 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,6 +11,10 @@ variable "subnet_id_1" {
   type = string
   default = "cloud-native-monitoring-app"
  }
+ variable "cluster_role_name" {
+  type = string
+  default = "cloud-native-cluster-role"
+ }
  
  variable "cluster_nodegroup_name" {
   type = string
